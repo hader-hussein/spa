4b4b896d9721653f69e5088c8122e98090ebdef7
hader-hussein <91197408+hader-hussein@users.noreply.github.com>
2024-01-06 11:08:21 +0200

    1-6

---
+++ teacher-Dashboard/assets/js/loade.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+     'use strict';
+     var windoh = $(window).height();
+     $('body').height(windoh);
+   
+    $(".component-stert  .component--intro ").fadeOut(2000,function()
+      {
+          $(this).parent().fadeOut(1000,function(){
+          $("body").css("overflow","auto");
+          $(this).remove();
+          $('body').height("100%");
+      });
+      
+    }
+    
+     );
+     const currentabout = location.href; 
+     const menuitemabout = document.querySelectorAll('.navber-index .nav-item ');
+      const menulengthabout = menuitemabout.length
+     for (let j = 0; j<menulengthabout; j++){
+      if(menuitemabout[j].href === currentabout) {
+       menuitemabout[j].className = "active" 
+         }
+     }
+         $('.navber-index .nav-item .nav-link').removeClass('active').removeAttr('aria-current');
+         $('a[href="' + location.pathname + '"]').closest('li').addClass('active').attr('aria-current', 'page');
+         $('.navber-index').css('background-color', '#f5f6f8');
+                
+   });
+
+$(window).scroll(function(){
+  $(' .navber-index').toggleClass('scrolled', $(this).scrollTop() > 50);
+  });
+
+// You can also pass an optional settings object
+// below listed default settings
+
+$(".counter").each(function () {
+  var $this = $(this),
+    countTo = $this.attr("data-countto");
+  countDuration = parseInt($this.attr("data-duration"));
+  $({ counter: $this.text() }).animate(
+    {
+      counter: countTo
+    },
+    {
+      duration: countDuration,
+      easing: "linear",
+      step: function () {
+        $this.text(Math.floor(this.counter));
+      },
+      complete: function () {
+        $this.text(this.counter);
+      }
+    }
+  );
+});
+
+/****/ 
+$(function(){
+  $('#people').multiSelect();
+});
+$(function(){
+  $('#peoplet').multiSelect();
+});
+$(function(){
+  $('#peopletr').multiSelect();
+});
+$('.rating').on('click', '.ratings_stars', function () {
+  var star = $(this)
+  star.addClass('selected')
+  star.prevAll().addClass('selected')
+  star.nextAll().removeClass('selected')
+  $('#rating').val(star.data('rating'))
+});
+const currentabout = location.href; 
+const menuitemabout = document.querySelectorAll('.navbar-dashboard .nav-link');
+ const menulengthabout = menuitemabout.length
+for (let j = 0; j<menulengthabout; j++){
+ if(menuitemabout[j].href === currentabout) {
+  menuitemabout[j].className = "active" 
+    }
+}
+    $('.navbar-dashboard .nav-link').removeClass('active').removeAttr('aria-current');
+    $('a[href="' + location.pathname + '"]').closest('li').addClass('active').attr('aria-current', 'page'); 
