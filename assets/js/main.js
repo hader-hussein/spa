7e3944397654d4e94dc9aedcfd4e3c3294f20d67
hader-hussein <91197408+hader-hussein@users.noreply.github.com>
2024-03-14 16:51:51 +0200

    14/3/2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@
             days[daysLen].addEventListener('click', function(){that.clickDay(this); });
         }
     };
-console.log(reset)
+
     Calendar.prototype.drawHeader = function(e) {
         var headDay = document.getElementsByClassName('head-day'),
             headMonth = document.getElementsByClassName('head-month');
@@ -167,3 +167,4 @@ console.log(reset)
 }, false);
 
 })(jQuery);
+/*clander-student*/
